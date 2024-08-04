b8e9db1fac1c387f045c74e7febffd20a9f74c7a
jorvixsky <jorvixsky@proton.me>
2024-08-04 09:17:35 +0200

    Now loading id's from chains objects

--- apps/web/src/hooks/useEAS.ts
+++ apps/web/src/hooks/useEAS.ts
@@ -1,13 +1,14 @@
 import { easBaseSepolia, easOptimismSepolia } from "@/lib/eas";
+import { optimismSepolia, baseSepolia } from "viem/chains";
 import { useChainId } from "wagmi";
 
 export default function useEAS() {
   const chainId = useChainId();
 
   switch (chainId) {
-    case 1:
+    case optimismSepolia.id:
       return easOptimismSepolia;
-    case 84532:
+    case baseSepolia.id:
       return easBaseSepolia;
     default:
       throw new Error(`Unsupported chainId: ${chainId}`);
