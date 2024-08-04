9f53fbce3c22e2044d67dce8796e3dff2fcb6e0b
jorvixsky <jorvixsky@proton.me>
2024-08-04 11:37:30 +0200

    Added enums

---
+++ apps/web/src/lib/enums.ts
@@ -0,0 +1,28 @@
+export enum Ratings {
+  Airplane = "Airplane",
+  Helicopter = "Helicopter",
+  PoweredParachute = "Powered Parachute",
+  Balloon = "Balloon",
+  SailPlane = "Sail Plane",
+  WeightShiftControl = "Weight-Shift Control",
+  Instrument = "Instrument",
+  Night = "Night",
+  Aerobatics = "Aerobatics",
+  Mountain = "Mountain",
+  FI = "FI",
+  IRA = "IRA",
+  Others = "Others",
+}
+
+export enum Licenses {
+  StudentPilot = "Student Pilot",
+  LAPL = "LAPL",
+  PPL = "PPL",
+  CPL = "CPL",
+  ATPL = "ATPL",
+  MPL = "MPL",
+  FlightInstructor = "Flight Instructor",
+  GroundInstructor = "Ground Instructor",
+  AircraftMechanic = "Aircraft Mechanic",
+  Others = "Others",
+}
