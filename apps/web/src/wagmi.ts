478311c61c36365c0a81860794ff48c912714ce3
jorvixsky <jorvixsky@proton.me>
2024-08-10 11:35:39 +0200

    Remove hardhat config

--- apps/web/src/wagmi.ts
+++ apps/web/src/wagmi.ts
@@ -1,10 +1,9 @@
 import { http, createConfig } from "wagmi";
-import { hardhat, baseSepolia, optimismSepolia } from "wagmi/chains";
+import { baseSepolia, optimismSepolia } from "wagmi/chains";
 
 export const wagmiConfig = createConfig({
-  chains: [hardhat, optimismSepolia, baseSepolia],
+  chains: [optimismSepolia, baseSepolia],
   transports: {
-    [hardhat.id]: http(),
     [optimismSepolia.id]: http(),
     [baseSepolia.id]: http(),
   },
