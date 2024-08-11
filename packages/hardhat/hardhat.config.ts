ac86c5a7a50b3d62247a234f92760f0650791bd6
jorvixsky <jorvixsky@proton.me>
2024-08-11 09:03:35 +0200

    Added config to deploy multiple chains and blockscout

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -14,12 +14,36 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY ?? "<your-api-key>",
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY ?? "<your-etherscan-api-key>",
+    },
+    customChains: [
+      {
+        network: "optimism-sepolia",
+        chainId: 11155420,
+        urls: {
+          apiURL: "https://optimism-sepolia.blockscout.com/api",
+          browserURL: "https://optimism-sepolia.blockscout.com/",
+        },
+      },
+      {
+        network: "base-sepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://base-sepolia.blockscout.com/api",
+          browserURL: "https://base-sepolia.blockscout.com/",
+        },
+      },
+    ],
   },
   networks: {
     hardhat: {},
     optimismSepolia: {
       url: process.env.ALCHEMY_URL_OP_SEPOLIA ?? "<your-alchemy-url>",
+      accounts: [process.env.PRIVATE_KEY ?? "<your-private-key>"],
+    },
+    baseSepolia: {
+      url: process.env.ALCHEMY_URL_BASE_SEPOLIA ?? "<your-base-sepolia-url>",
       accounts: [process.env.PRIVATE_KEY ?? "<your-private-key>"],
     },
   },
