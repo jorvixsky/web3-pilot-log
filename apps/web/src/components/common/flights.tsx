f830205fdd7f2bad024392525eca752b11ee0e9b
Pingu <pinglupingu@proton.me>
2024-08-11 10:16:05 +0200

    fix merge errors

--- apps/web/src/components/common/flights.tsx
+++ apps/web/src/components/common/flights.tsx
@@ -13,40 +13,12 @@ const columns: ColumnDef<any>[] = [
     header: "Departure",
   },
   {
-    accessorKey: "departure.time",
-    header: "Departure Time",
-  },
-  {
     accessorKey: "arrival.place",
     header: "Arrival",
   },
   {
     accessorKey: "aircraft.model",
     header: "Aircraft",
-  },
-  {
-    accessorKey: "totalTimeOfFlight.hours",
-    header: "Total Time of Flight",
-  },
-  {
-    accessorKey: "numberOfLandings.day",
-    header: "Number of Landings",
-  },
-  {
-    accessorKey: "numberOfLandings.night",
-    header: "Number of Landings",
-  },
-  {
-    accessorKey: "remarks",
-    header: "Remarks",
-  },
-  {
-    accessorKey: "selfSigned",
-    header: "Self Signed",
-  },
-  {
-    accessorKey: "signedBy",
-    header: "Signed By",
   },
   {
     accessorKey: "pics",
